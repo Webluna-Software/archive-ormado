3eaa7b55ec9994e8057b208a285be7784994b91a
Hasan Bakhtiar <hasan.doctype@gmail.com>
2024-03-08 22:57:30 +0400

    ormado v0.9.7 fixed

--- src/pages/Franchise.jsx
+++ src/pages/Franchise.jsx
@@ -7,17 +7,33 @@ import Faq from '../components/home/Faq'
 import cardimg from '../assets/img/first.png'
 import { testimonalsforfranchise } from '../data/data'
 const Franchise = () => {
-    const fileInputRef = useRef(null);
-  
-    const handleClick = () => {
-      fileInputRef.current.click();
-    };
+  const fileInputRef = useRef(null);
+
+  const handleClick = () => {
+    w
+    fileInputRef.current.click();
+  };
   return (
     <div >
+      <section className="franchise-mobile d-none" >
+      <div className="bg-clr">
+      <p>
+          WHY <span className="ormado">ORMADO</span> FRANCHISE ?
+        </p>
+        <p>
+          If you're considering starting your own business in the coffee industry, Ormado Kaffeehause can be the best choice, offering comprehensive knowledge from A to Z.
+        </p>
+        <div className="ormado-button">
+              <Link to="/franchiseform"><button className="ormado-button">INQUIRE NOW</button></Link>
+            </div>
+      </div>
+
+      </section>
       <section className="myfranchise">
+      
         <div className="franc-con">
           <div className="left">
-           
+
             <div className="text">
               <p>
                 WHY <span className="ormado">ORMADO</span> FRANCHISE ?
@@ -26,39 +42,40 @@ const Franchise = () => {
 
             <div className="ormado-about">
               <p>
-              If you're considering starting your own business in the coffee industry, Ormado Kaffeehause can be the best choice, offering comprehensive knowledge from A to Z.
+                If you're considering starting your own business in the coffee industry, Ormado Kaffeehause can be the best choice, offering comprehensive knowledge from A to Z.
               </p>
             </div>
-            <div className="ormado-button">  
+            <div className="ormado-button">
               <Link to="/franchiseform"><button>INQUIRE NOW</button></Link>
-            </div>     
+            </div>
           </div>
-       
+
           <div className="right">
-          <img className="img-fluid" src={franchise} alt="" />
+            <img className="img-fluid" src={franchise} alt="" />
           </div>
         </div>
         <div className="roasting">
-        <h3 className="ms-1 my-5">
-           <font color="#D59729">Coffee</font>
-        </h3>
-        <p className="ms-1 w-50">
-        As Ormado Kaffeehaus, we offer coffees with a unique composition. If you also want to own a coffee shop that offers quality coffee with a unique taste to your customers, our franchise is exactly what you are looking for.
-        </p>
-    
-        <div className="franchise-cards mb-5">
-          <div className="row">
-            <img className='img' src={cardimg} alt="" />
-            <img src={cardimg} alt="" />
-            <img src={cardimg} alt="" />
+          <h3 className="ms-1 my-5">
+            <font color="#D59729">Coffee</font>
+          </h3>
+          <p className="ms-1 w-50">
+            As Ormado Kaffeehaus, we offer coffees with a unique composition. If you also want to own a coffee shop that offers quality coffee with a unique taste to your customers, our franchise is exactly what you are looking for.
+          </p>
+
+          <div className="franchise-cards mb-5">
+            <div className="row">
+              <img className='img' src={cardimg} alt="" />
+              <img src={cardimg} alt="" />
+              <img src={cardimg} alt="" />
+            </div>
           </div>
         </div>
-      </div>
-     
-    </section>
-  
- 
-    <Testimonials senddata={testimonalsforfranchise} />
+
+      </section>
+
+
+
+      <Testimonials senddata={testimonalsforfranchise} />
       <div className="franchisecompany">
         <div className="franchisecompany-part1-text mb-2">
           <p>Are you fascinated by the Kaffeehaus vibes? Do you want to build your own business?</p>
@@ -106,15 +123,15 @@ const Franchise = () => {
           <div className="part1">
             <p>You can download the presentation for more details.</p>
             <form>
-      <button className='button' onClick={handleClick}>
-        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path opacity="0.5" fill-rule="evenodd" clip-rule="evenodd" d="M3 14.25C3.41421 14.25 3.75 14.5858 3.75 15C3.75 16.4354 3.75159 17.4365 3.85315 18.1919C3.9518 18.9257 4.13225 19.3142 4.40901 19.591C4.68577 19.8678 5.07435 20.0482 5.80812 20.1469C6.56347 20.2484 7.56459 20.25 9 20.25H15C16.4354 20.25 17.4365 20.2484 18.1919 20.1469C18.9257 20.0482 19.3142 19.8678 19.591 19.591C19.8678 19.3142 20.0482 18.9257 20.1469 18.1919C20.2484 17.4365 20.25 16.4354 20.25 15C20.25 14.5858 20.5858 14.25 21 14.25C21.4142 14.25 21.75 14.5858 21.75 15V15.0549C21.75 16.4225 21.75 17.5248 21.6335 18.3918C21.5125 19.2919 21.2536 20.0497 20.6517 20.6516C20.0497 21.2536 19.2919 21.5125 18.3918 21.6335C17.5248 21.75 16.4225 21.75 15.0549 21.75H8.94513C7.57754 21.75 6.47522 21.75 5.60825 21.6335C4.70814 21.5125 3.95027 21.2536 3.34835 20.6517C2.74643 20.0497 2.48754 19.2919 2.36652 18.3918C2.24996 17.5248 2.24998 16.4225 2.25 15.0549C2.25 15.0366 2.25 15.0183 2.25 15C2.25 14.5858 2.58579 14.25 3 14.25Z" fill="#ffffff" />
-          <path fill-rule="evenodd" clip-rule="evenodd" d="M12 16.75C12.2106 16.75 12.4114 16.6615 12.5535 16.5061L16.5535 12.1311C16.833 11.8254 16.8118 11.351 16.5061 11.0715C16.2004 10.792 15.726 10.8132 15.4465 11.1189L12.75 14.0682V3C12.75 2.58579 12.4142 2.25 12 2.25C11.5858 2.25 11.25 2.58579 11.25 3V14.0682L8.55353 11.1189C8.27403 10.8132 7.79963 10.792 7.49393 11.0715C7.18823 11.351 7.16698 11.8254 7.44648 12.1311L11.4465 16.5061C11.5886 16.6615 11.7894 16.75 12 16.75Z" fill="#ffffff" />
-        </svg>
-        <p>Franchise Presentation EN</p>
-      </button>
-      <input type="file" id='file' style={{ display: 'none' }} ref={fileInputRef} />
-    </form>
+              <button className='button' onClick={handleClick}>
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path opacity="0.5" fill-rule="evenodd" clip-rule="evenodd" d="M3 14.25C3.41421 14.25 3.75 14.5858 3.75 15C3.75 16.4354 3.75159 17.4365 3.85315 18.1919C3.9518 18.9257 4.13225 19.3142 4.40901 19.591C4.68577 19.8678 5.07435 20.0482 5.80812 20.1469C6.56347 20.2484 7.56459 20.25 9 20.25H15C16.4354 20.25 17.4365 20.2484 18.1919 20.1469C18.9257 20.0482 19.3142 19.8678 19.591 19.591C19.8678 19.3142 20.0482 18.9257 20.1469 18.1919C20.2484 17.4365 20.25 16.4354 20.25 15C20.25 14.5858 20.5858 14.25 21 14.25C21.4142 14.25 21.75 14.5858 21.75 15V15.0549C21.75 16.4225 21.75 17.5248 21.6335 18.3918C21.5125 19.2919 21.2536 20.0497 20.6517 20.6516C20.0497 21.2536 19.2919 21.5125 18.3918 21.6335C17.5248 21.75 16.4225 21.75 15.0549 21.75H8.94513C7.57754 21.75 6.47522 21.75 5.60825 21.6335C4.70814 21.5125 3.95027 21.2536 3.34835 20.6517C2.74643 20.0497 2.48754 19.2919 2.36652 18.3918C2.24996 17.5248 2.24998 16.4225 2.25 15.0549C2.25 15.0366 2.25 15.0183 2.25 15C2.25 14.5858 2.58579 14.25 3 14.25Z" fill="#ffffff" />
+                  <path fill-rule="evenodd" clip-rule="evenodd" d="M12 16.75C12.2106 16.75 12.4114 16.6615 12.5535 16.5061L16.5535 12.1311C16.833 11.8254 16.8118 11.351 16.5061 11.0715C16.2004 10.792 15.726 10.8132 15.4465 11.1189L12.75 14.0682V3C12.75 2.58579 12.4142 2.25 12 2.25C11.5858 2.25 11.25 2.58579 11.25 3V14.0682L8.55353 11.1189C8.27403 10.8132 7.79963 10.792 7.49393 11.0715C7.18823 11.351 7.16698 11.8254 7.44648 12.1311L11.4465 16.5061C11.5886 16.6615 11.7894 16.75 12 16.75Z" fill="#ffffff" />
+                </svg>
+                <p>Franchise Presentation EN</p>
+              </button>
+              <input type="file" id='file' style={{ display: 'none' }} ref={fileInputRef} />
+            </form>
           </div>
           <div className="part2">
             <p>If you still haven't found answers to the questions that interest you, then fill out the forum and we will contact you!</p>
@@ -123,6 +140,8 @@ const Franchise = () => {
         </div>
       </div>
       <Faq />
+    
+
     </div>
   )
 }
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -36,9 +36,9 @@ const Products = () => {
       <section className="container-fluid">
         <div className="products-page row pb-5 my-5 d-flex justify-content-center">
       
-
           <div className="col-md-9">
-            <div className="img-transparent d-none d-md-block d-lg-block">
+            <div className="img-transparent  d-md-block d-lg-block">
+
               <div className="image-container">
                 <img
                   src={productImg}
@@ -50,7 +50,7 @@ const Products = () => {
             </div>
 
         
-            <div className="product-cards d-none d-md-block d-lg-block p-0 mt-5">
+            <div className="product-cards  d-md-block d-lg-block p-0 mt-5">
               <div className="yourormado-products row g-3">
                 {products.map((fd,i)=>(
                   <div className="col-12 col-sm-6 col-md-4">
