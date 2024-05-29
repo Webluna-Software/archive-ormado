2ff7ff28a687a6f4e0caa2a1b5ed4ddfbf1bf122
Mamedova Ayishan <mamedovaayisn@gmail.com>
2024-05-29 12:03:28 +0400

    day edit 1

--- src/pages/PreLoader.jsx
+++ src/pages/PreLoader.jsx
@@ -1,13 +1,12 @@
 import React from "react";
-// import logo from "../assets/img/Logo.png";
+import logo from "../assets/img/Logo.png";
 const PreLoader = () => {
   return (
     <>
       <div className="d-flex justify-content-center align-items-center divdiv">
-        {/* <img src={logo} alt="" className="img-fluid  rotate-animation" /> */}
-        {/* <div className="preloader"></div> */}
-        <div className="preloader">
-    </div>
+        <img src={logo} alt="" className="img-fluid  rotate-animation" />
+
+
       </div>
     
     </>
