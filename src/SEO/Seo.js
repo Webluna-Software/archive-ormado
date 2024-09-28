44b53ea0f1b24b7e273be053095cc8ca25014f92
Tamerlan <daniels-95@inbox.ru>
2024-09-28 15:19:41 +0400

    career faq / gallery faq/ active / seo

--- src/SEO/Seo.js
+++ src/SEO/Seo.js
@@ -1,22 +1,22 @@
 
-(function () {
-    fetch("https://ormadoapi.webluna.org/api/client/mainSetting", {
-      method: "GET",
-    })
-      .then((response) => response.json())
-      .then((res) => {
+// (function () {
+//     fetch("https://ormadoapi.webluna.org/api/client/mainSetting", {
+//       method: "GET",
+//     })
+//       .then((response) => response.json())
+//       .then((res) => {
   
-        const metaDescription = document.getElementById("description");
-        const metaKeywords = document.getElementById("keywords");
+//         const metaDescription = document.getElementById("description");
+//         const metaKeywords = document.getElementById("keywords");
   
-        const mainDescription = JSON.stringify(res.data[0].description);
-        const mainKeyWords = JSON.stringify(res.data[0].seoKeywords);
+//         const mainDescription = JSON.stringify(res.data[0].description);
+//         const mainKeyWords = JSON.stringify(res.data[0].seoKeywords);
   
-        metaDescription.setAttribute("content", mainDescription);
-        metaKeywords.setAttribute("content", mainKeyWords);
-      })
+//         metaDescription.setAttribute("content", mainDescription);
+//         metaKeywords.setAttribute("content", mainKeyWords);
+//       })
       
-      .catch((error) => {
-        console.error("API XƏTA BAŞ VERDİ", error);
-      });
-  })();  
+//       .catch((error) => {
+//         console.error("API XƏTA BAŞ VERDİ", error);
+//       });
+//   })();  
--- src/components/home/Faq.jsx
+++ src/components/home/Faq.jsx
@@ -2,71 +2,73 @@ import axios from "axios";
 import { useContext, useEffect, useState } from "react";
 import ApiLinkContext from "../../context/ApiLinkContext";
 
-
-const Faq = ({faqs}) => {
-
+const Faq = ({ faqs }) => {
+  const active = faqs.map((item)=>item.active)
   return (
     <>
-     
-        <div className="Faqs">
-          <div className="container1">
-            <div className="Center">
-              <div className="title">
-                <div className="line"></div>
-                <div className="upper">
-                  <h5>FAQ</h5>
-                </div>
+      {
+        active 
+        == true && faqs.length > 0 && (
+          <div className="Faqs">
+        <div className="container1">
+          <div className="Center">
+            <div className="title">
+              <div className="line"></div>
+              <div className="upper">
+                <h5>FAQ</h5>
               </div>
+            </div>
 
-              <div className="question">
-                <h1>
-                  Questions ? <span>Look here</span>
-                </h1>
-              </div>
+            <div className="question">
+              <h1>
+                Questions ? <span>Look here</span>
+              </h1>
+            </div>
 
-              <div className="accordion " id="accordionExample">
-                {faqs.map((item, i) => {
-                  return (
-                    <div
-                      className="accordion-item"
-                      id={`first${i}`}
-                      key={item._id}
-                    >
-                      <h2 className="accordion-header">
-                        <button
-                          id={i}
-                          className="accordion-button collapsed"
-                          type="button"
-                          data-bs-toggle="collapse"
-                          data-bs-target={`#collapse${i}`}
-                          aria-expanded="false"
-                          aria-controls={`collapse${i}`}
-                        >
-                          <h6> {item.title} </h6>
-                        </button>
-                      </h2>
-                      <div
-                        id={`collapse${i}`}
-                        className={`accordion-collapse collapse ${
-                          i == 0 ? "show" : ""
-                        } `}
-                        data-bs-parent="#accordionExample"
+            <div className="accordion " id="accordionExample">
+              {faqs.map((item, i) => {
+                return  ( 
+                  <div
+                    className="accordion-item"
+                    id={`first${i}`}
+                    key={item._id}
+                  >
+                    <h2 className="accordion-header">
+                      <button
+                        id={i}
+                        className="accordion-button collapsed"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target={`#collapse${i}`}
+                        aria-expanded="false"
+                        aria-controls={`collapse${i}`}
                       >
-                        <div className="accordion-body  ">
-                          <p
-                            className="lorem1 "
-                            dangerouslySetInnerHTML={{ __html: item.text }}
-                          />
-                        </div>
+                        <h6>{item.title}</h6>
+                      </button>
+                    </h2>
+                    <div
+                      id={`collapse${i}`}
+                      className={`accordion-collapse collapse ${
+                        i == 0 ? "show" : ""
+                      }`}
+                      data-bs-parent="#accordionExample"
+                    >
+                      <div className="accordion-body">
+                        <p
+                          className="lorem1"
+                          dangerouslySetInnerHTML={{ __html: item.text }}
+                        />
                       </div>
                     </div>
-                  );
-                })}
-              </div>
+                  </div>
+                ) 
+              })}
             </div>
           </div>
         </div>
-     
+      </div>
+        )
+      }
     </>
   );
 };
