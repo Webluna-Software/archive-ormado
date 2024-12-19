ccc8faf7e4836479ae8410dfd7df90c3361cfa10
Mamedova Ayishan <mamedovaayisn@gmail.com>
2024-12-19 12:43:28 +0400

    eventdetails youtube shorts video

--- src/pages/EventDetails.jsx
+++ src/pages/EventDetails.jsx
@@ -3,7 +3,6 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 import { ApiLinkContext } from "../context/ApiLinkContext";
 import PreLoader from "./PreLoader";
-// import { Helmet } from "react-helmet";
 import LazyLoad from "react-lazy-load";
 
 const EventDetails = () => {
@@ -13,10 +12,9 @@ const EventDetails = () => {
   const { ApiLink2 } = useContext(ApiLinkContext);
 
   useEffect(() => {
-    // console.log("Event ID from URL:", id);
-    axios.get(`${ApiLink2}/event/${id}`)
+    axios
+      .get(`${ApiLink2}/event/${id}`)
       .then((res) => {
-        // console.log("API Response:", res.data.data);
         setEventDetails(res.data.data);
         setLoading(false);
       })
@@ -25,38 +23,32 @@ const EventDetails = () => {
         setLoading(false);
       });
   }, [id, ApiLink2]);
-  
 
-  const getYoutubeEmbedUrl = (url) => {
+ const getYoutubeEmbedUrl = (url) => {
     let videoId = null;
 
     if (url.includes("youtu.be")) {
-      videoId = url.split("youtu.be/")[1].split("?")[0];
+        // Shortened URL
+        videoId = url.split("youtu.be/")[1].split("?")[0];
     } else if (url.includes("youtube.com")) {
-      const params = new URLSearchParams(url.split("?")[1]);
-      videoId = params.get("v");
+        if (url.includes("/shorts/")) {
+            // Shorts URL
+            videoId = url.split("/shorts/")[1].split("?")[0];
+        } else {
+            // Standard YouTube URL
+            const params = new URLSearchParams(url.split("?")[1]);
+            videoId = params.get("v");
+        }
     }
 
     if (videoId) {
-      return `https://www.youtube.com/embed/${videoId}`;
+  
+        return `https://www.youtube.com/embed/${videoId}?autoplay=1&mute=1&controls=1&modestbranding=1&rel=0&showinfo=0&autohide=1`;
     }
-    return "";
-  };
+    return null;
+};
 
-  //  // Mətnin boşluqla bitməməsini yoxlayan funksiya
-  //  const isValidText = (text) => {
-  //   return text && text.trim() !== "";
-  // };
-
-  // // Şəkil URL-inin düzgünlüyünü yoxlayan funksiya
-  // const isValidImageUrl = (url) => {
-  //   return url && url.trim() !== "" && url !== "undefined";
-  // };
-
-  // // Video URL-inin düzgünlüyünü yoxlayan funksiya
-  // const isValidVideoUrl = (url) => {
-  //   return url && url.trim() !== "" && url !== "undefined" && getYoutubeEmbedUrl(url);
-  // };
+  
 
   return (
     <>
@@ -64,46 +56,52 @@ const EventDetails = () => {
         {loading ? (
           <PreLoader />
         ) : (
-          <>
-               <hr style={{ color: "orange", fontWeight: "bold" }} />
-            <div className="container-fluid ">
-              <div className="row">
-                <div className="event-details">
-                    <div className="event-title">
-                        <h3>{eventDetails.title}</h3>
+          <div className="container-fluid">
+            <hr style={{ color: "orange", fontWeight: "bold" }} />
+            <div className="row">
+              <div className="event-details">
+                {eventDetails.title && (
+                  <div className="event-title">
+                    <h3>{eventDetails.title}</h3>
+                  </div>
+                )}
+                {eventDetails.image && (
+                  <div className="event-img">
+                    <LazyLoad>
+                      <img
+                        src={eventDetails.image}
+                        alt={eventDetails.title}
+                        className="img-fluid w-100"
+                      />
+                    </LazyLoad>
+                  </div>
+                )}
+                {eventDetails.desc && eventDetails.desc.trim() !== "" && (
+                  <div className="my-5 event-text">
+                    <p
+                      dangerouslySetInnerHTML={{
+                        __html: eventDetails.desc.trim(),
+                      }}
+                    />
+                  </div>
+                )}
+                {eventDetails.video &&
+                  eventDetails.video.trim() !== "" &&
+                  getYoutubeEmbedUrl(eventDetails.video) && (
+                    <div className="event-video">
+                      <iframe
+                        width="100%"
+                        height="400px"
+                        src={getYoutubeEmbedUrl(eventDetails.video)}
+                        frameBorder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        allowFullScreen
+                      ></iframe>
                     </div>
-                <div className="event-img">
-                            <LazyLoad>
-                              <img
-                                src={eventDetails.image}
-                                alt={eventDetails.title}
-                                className="img-fluid w-100"
-                              />
-                            </LazyLoad>
-                          </div>
-                        <div className="my-5 event-text">
-                          <p
-                            dangerouslySetInnerHTML={{
-                              __html: eventDetails.desc,
-                            }}
-                          />
-                        </div>
-                        {eventDetails.video && eventDetails.video.trim() !== "" && eventDetails.video !== "undefined" && (
-                           <div className="event-video">
-                             <iframe
-                               width="100%"
-                               height="400px"
-                               src={getYoutubeEmbedUrl(eventDetails.video)}
-                               frameBorder="0"
-                               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                               allowFullScreen
-                             ></iframe>
-                           </div>
-                        )}
-                </div>
+                  )}
               </div>
             </div>
-          </>
+          </div>
         )}
       </section>
     </>
